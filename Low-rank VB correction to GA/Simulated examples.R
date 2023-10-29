06a65cf8b0cc73140f4a0542903585a29761203c
JanetVN1201 <58976783+JanetVN1201@users.noreply.github.com>
2023-10-29 10:47:56 +0300

    Replace with added examples

--- Low-rank VB correction to GA/Simulated examples.R
+++ Low-rank VB correction to GA/Simulated examples.R
@@ -1,5 +1,5 @@
 library(tictoc)
-
+library(mvtnorm)
 ###Example in section 3.2
 
 x0 <- seq(-10,5,by = 0.001)
@@ -8,148 +8,158 @@ f1 <- function(x){
   a = aa
   ll = 0
   for (i in 1:N){
-    ll = ll + dpois(a[i], lambda = exp(x[1] + x[2]*cov[i]), log = T) 
+    ll = ll + dpois(a[i], lambda = exp(x[1] + x[2]*cov[i] + x[i+2] - mean(x)), log = T)
   }
-  ll = ll + dt(x[1], df = 5, log = TRUE) + dunif(x[2], min = -3, max = 3, log = TRUE) 
+  ll = ll + dt(x[2], df = 5, log = TRUE) + dunif(x[1], min = -3, max = 3, log = T) + dmvnorm(x[1:N+2], mean = rep(0, N), sigma = 0.25*diag(N), log = T)
   return(ll)
-}
+  }
+
 
 mu = -1
 beta1 = -0.5
-N = 20
+N = 100
+x_i = rnorm(N, mean = 0, sd = 0.5)
 cov = rnorm(N)
-aa = rpois(N, lambda  = exp(mu + beta1*cov))
+aa = rpois(N, lambda  = exp(mu + beta1*cov + x_i))
 par(mar = c(4,2,2,1))
 
 barplot(table(as.factor(aa)), main = "", ylim = c(0, N))
 
 tic("Gaussian")
 if (T){
- tol = 2
- mode1 = c(4,1)
- while (tol>0.001){
- hess2 <- numDeriv::hessian(func=f1, x = mode1)
- grad2 <- numDeriv::grad(func=f1, x = mode1)
- mode2 = solve(-hess2, grad2-hess2%*%mode1)
- tol = sum(abs(mode1 - mode2))
- mode1 = mode2
- }
+  tol = 1000
+  mode1 = c(-1,-0.5, rep(0, N))
+  while (tol>10){
+    hess2 <- numDeriv::hessian(func=f1, x = mode1)
+    grad2 <- numDeriv::grad(func=f1, x = mode1)
+    mode2 = solve(-hess2, grad2-hess2%*%mode1)
+    tol = sum(abs(mode1 - mode2))
+    mode1 = mode2
+  }
 }
-
-hess2 <- numDeriv::hessian(func=f1, x = mode1)
-m = mode1
 toc()
+
+m = mode1
+
 cov1 = solve(-hess2)
+
+n_corr = 2
+
+u_A = diag(N)
+A = matrix(data = cbind(rep(1, N), cov, u_A),
+                            ncol = 2+N)
+cov_eta = A%*%cov1%*%t(A)
 
 minf = function(delta) {
   #Loglik
-  A = matrix(data = cbind(rep(1, N), cov), ncol = 2)
-  eta = matrix(data = cbind(rep(1, N), cov), ncol = 2)%*%(m + delta)
-  cov_eta = A%*%cov1%*%t(A)
-  
+  eta = A%*%(m + cov1[,1:n_corr]%*%delta)
   ll = 0
   for (i in 1:N){
-  loglik = exp(eta[i] + cov_eta[i,i]/2) - 
-    aa[i]*eta[i]
-  ll = ll + loglik}
+    loglik = exp(eta[i] + cov_eta[i,i]/2) - 
+      aa[i]*eta[i]
+    ll = ll + loglik}
   
   #KLD
-  xx = rbind(rep(seq(-3,  1, length = 20), each = 20),rep(seq( - 3,  3, length = 20), n = 20))
+  xx = rbind(rep(seq(-3,  3, length = 20), each = 100),rep(seq( - 3,  3, length = 20), n = 100))
   area = (max(xx[1,]) - min(xx[1,]))*(max(xx[2,]) - min(xx[2,]))
   prior = rep(NA, ncol(xx))
   post = prior
   f = prior
   f2 = prior
   for (i in 1:ncol(xx)){
-  prior[i] = dt(xx[1,i], df = 5)*dunif(xx[2,i], min = -3, max = 3)#dnorm(xx[1,i], mean = 0, sd = 1, log = FALSE)#dnorm(xx[1,i], mean = 0, sd = 1, log = F)*dnorm(xx[2,i], mean = 0, sd = 1, log = F)
-  post[i] = dmvnorm(xx[,i], mean = m + delta, sigma = cov1, log = F)
-  f[i] = post[i]*(log(post[i]+0.001) - log(prior[i]+0.001))
+    prior[i] = dt(xx[2,i], df = 5)*dunif(xx[1,i], min = -3, max = 3)
+    post[i] = dmvnorm(xx[,i], mean = m[1:n_corr] + cov1[1:n_corr,1:n_corr]%*%delta, sigma = cov1[1:n_corr,1:n_corr], log = F)
+    f[i] = post[i]*(log(post[i]+0.001) - log(prior[i]+0.001))
   }
-  kl = area*sum(f)/ncol(xx)
+  kl = 2*area*sum(f)/ncol(xx)
   return (ll + kl)
 }
 
 
 tic("VBC")
-d_est = optim(c(0,0), minf)
+d_est = optim(rep(0,n_corr), minf)
 toc()
-d_est$par
-improved_mean = m + d_est$par
-improved_mean_eta = cbind(rep(1, N), cov)%*%m + cbind(rep(1, N), cov)%*%d_est$par
-delta_eta = cbind(rep(1, N), cov)%*%d_est$par
-
-par(mar = c(2,2,1,1))
-plot(c(delta, delta_eta), pch = c(rep(2,2),rep(16, N)), cex = c(rep(1,2), rep(0.5, N)), xaxt = "n", xlab = "", ylab = expression(paste(delta)),
-     ylim = c(-0.2, 0))
-abline(h=0, lty = 2)
-axis(1, at=1:(N+2), labels=c(expression(paste(beta[0])), 
-                             expression(paste(beta[1])),
-                             expression(paste(eta[1])),
-                             expression(paste(eta[2])),
-                             expression(paste(eta[3])),
-                             expression(paste(eta[4])),
-                             expression(paste(eta[5])),
-                             expression(paste(eta[6])),
-                             expression(paste(eta[7])),
-                             expression(paste(eta[8])),
-                             expression(paste(eta[9])),
-                             expression(paste(eta[10])),
-                             expression(paste(eta[11])),
-                             expression(paste(eta[12])),
-                             expression(paste(eta[13])),
-                             expression(paste(eta[14])),
-                             expression(paste(eta[15])),
-                             expression(paste(eta[16])),
-                             expression(paste(eta[17])),
-                             expression(paste(eta[18])),
-                             expression(paste(eta[19])),
-                             expression(paste(eta[20]))))
-
+delta = d_est$par
+improved_mean = m + cov1[,1:2]%*%d_est$par
+improved_mean_eta = A%*%(m + (cov1[,1:2]%*%d_est$par))
+delta_eta = A%*%(cov1[,1:2]%*%d_est$par)
 
 
 #MCMC
-  model = "model {
-  for(i in 1:n){
-    y[i] ~ dpois(lambda[i])
-    log(lambda[i]) = beta0 + beta1*x[i]
-  }
 
-  beta0 ~ dt(0,1,5)
-  beta1 ~ dunif(-3,3)
+model = "model {
+  for(i in 1:N){
+    y[i] ~ dpois(lambda[i])
+    log(lambda[i]) = beta0 + beta1*x[i] + ustar[i]
+}
+  beta0 ~ dunif(-3,3)
+  beta1 ~ dt(0,1,5)
+  for (j in 1:N){
+  u[j] ~ dnorm(0,4)
+  ustar[j] <- u[j] - mean(u[])
+  }
+}"
+
+model = "model {
+  for(i in 1:N){
+    y[i] ~ dpois(lambda[i])
+    log(lambda[i]) = beta0 + beta1*x[i] + u[i]
+}
+  beta0 ~ dunif(-3,3)
+  beta1 ~ dt(0,1,5)
+  for (j in 1:N){
+  u[j] ~ dnorm(0,4)
+  }
 }"
 
 library(runjags)
-  tic("MCMC")
+tic("MCMC")
 res.1 = run.jags(model = model,
-                 monitor = c("beta0", "beta1"),
-                 data = list(y = aa, n = N, x = cov),
-                 n.chains = 6,
-                 inits = list(beta0 = -1, beta1 = -0.5),
-                 burnin = 10^2,
+                 monitor = c("beta0","beta1", "u[1]", "u[8]","u[15]"),
+                 data = list('y' = aa, 'x' = cov, 'N' = N),
+                 n.chains = 1,
+                 inits = list(beta0 = 1, beta1 = -0.5, u = rep(0.5,N)),
+                 burnin = 10^3,
                  sample = 10^5,
                  method = "parallel")
 
 trace <- combine.mcmc(res.1)
 toc()
-mean.mcmc <- c(mean(trace[, "beta0"]), mean(trace[, "beta1"]))
+mean.mcmc <- c(mean(trace[, "beta0"]), mean(trace[, "beta1"]), mean(trace[,"u[1]"]), mean(trace[,"u[8]"]), mean(trace[,"u[15]"]))
 print(mean.mcmc)
 
-dev.off()
-par(mar = c(4,2,1,1))
-hist(trace[, "beta0"], col = "azure1", prob = T, breaks = 50, xlab = expression(paste(beta[0])), main = "", ylab = "")
-lines(x0, dnorm(x0, mean = mode1[1], sd = sqrt(cov1[1,1]), log = F), lty = 2, col = "black", lwd = 2)
-lines(x0,dnorm(x0, mean = improved_mean[1], sd = sqrt(cov1[1,1]), log = F), lty = 1, col = "blue", lwd = 2)
-lines(x0, dt(x0, 0, df = 5), lty = 4, lwd = 2, col = "red")
+####Stan using BRMS
+library(rstan)
+library(brms)
+library(tidybayes)
+library(dplyr)
 
-hist(trace[, "beta1"], col = "azure1", prob = T, breaks = 50, xlab = expression(paste(beta[1])), main = "", ylab = "")
-lines(x0, dnorm(x0, mean = mode1[2], sd = sqrt(cov1[2,2]), log = F), lty = 2, col = "black", lwd = 2)
-lines(x0,dnorm(x0, mean = improved_mean[2], sd = sqrt(cov1[2,2]), log = F), lty = 1, col = "blue", lwd = 2)
-lines(x0, dunif(x0, min = -3, max = 3), lty = 4, lwd = 2, col = "red")
 
-res= rbind(t(m), t(improved_mean), t(mean.mcmc))
-colnames(res) = c("beta0", "beta1")
-rownames(res) = c("gaussian", "vb.corrected", "mcmc")
-print(res)
+tic("STAN")
+sv <- stanvar(scode = "sum(z_1[1])~normal(0, N_1*0.001);",
+              block = "model", position = "end")
+
+mod_sim <- brm(y ~ 1 + xx + (1|group),
+               family = poisson,
+               data = data.frame(y = aa, xx = cov, group = 1:N),
+               refresh = 0, silent = 2, cores = 4,
+               prior = c(
+                 prior(normal(0,1), class = Intercept),
+                 prior(normal(0.5,0.001), class = sd)
+               ),
+               stanvars = sv,
+               warmup = 10000, iter = 100000)
+toc()
+
+samplesss <- data.frame(mod_sim %>%
+  spread_draws(r_group[group,]))
+fixed_e <- data.frame(mod_sim %>%
+                        spread_draws(c(b_Intercept,b_xx)))
+
+stan_sam <- matrix(data = rep(NA, 360000*N/num_pg), ncol = N/num_pg, nrow = 360000)
+for (i in 1:(N/num_pg)){
+  stan_sam[,i] <- samplesss[samplesss$group == i, 2]
+}
 
 ###########Example in Section 4.3
 ##MCMC - define
